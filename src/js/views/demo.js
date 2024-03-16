c593e657617072d5e58be628d2851f61fa4d627c
Anibal-JPG <109487916+Anibal-JPG@users.noreply.github.com>
2024-03-16 04:47:11 +0000

    crear usuario y que el form quede vacio

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -7,60 +7,87 @@ import "../../styles/demo.css";
 
 export const Demo = () => {
 
-	const [contact, setContact] = useState()
+	const [contact, setContact] = useState({
+		"full_name": "",
+		"email": "",
+		"agenda_slug": "my_super_agenda",
+		"address": "",
+		"phone": ""
+	})
 
-	const apisubmmit = async () => {
+	const apisubmmit = async (event) => {
+		event.preventDefault()
 		try {
-			const api = await fetch("https://playground.4geeks.com/apis/fake/contact/")
+			const api = await fetch("https://playground.4geeks.com/apis/fake/contact", {
+				method: "POST",
+				body: JSON.stringify(contact),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
 			const data = await api.json();
 			console.log(data)
-			setContact(data)
+			setContact({
+				"full_name": "",
+				"email": "",
+				"agenda_slug": "my_super_agenda",
+				"address": "",
+				"phone": ""
+			})
 
 		} catch (error) {
 			console.log(error)
 		}
-
-
 	}
 
-	return <div>
+
+
+	useEffect(() => {
+		console.log(contact)
+	}, [contact])
+
+
+
+	return <>
 
 		<div style={{ textAlign: "center" }}>
 			<h1>Add a New Contact</h1>
 
-			<div className="container">
-				<div class="row d-block">
-					<label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Full Name</label>
-					<div class="col-sm-10">
-						<input type="email" class="form-control form-control-lg colorStyle" style={{ width: "1235px" }} id="colFormLabelLg" placeholder="Enter Full name" />
+			<form onSubmit={apisubmmit}>
+				<div className="container">
+					<div class="row d-block">
+						<label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Full Name</label>
+						<div class="col-sm-10">
+							<input value={contact.full_name} type="text" onChange={(event) => setContact({ ...contact, full_name: event.target.value })} class="form-control form-control-lg colorStyle" style={{ width: "1235px" }} id="colFormLabelLg" placeholder="Enter Full name" />
+						</div>
 					</div>
-				</div>
-				<div class="row d-block">
-					<label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Email</label>
-					<div class="col-sm-10">
-						<input type="email" class="form-control form-control-lg colorStyle" style={{ width: "1235px" }} id="colFormLabelLg" placeholder="Enter Email" />
+					<div class="row d-block">
+						<label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Email</label>
+						<div class="col-sm-10">
+							<input value={contact.email} type="email" onChange={(event) => setContact({ ...contact, email: event.target.value })} class="form-control form-control-lg colorStyle" style={{ width: "1235px" }} id="colFormLabelLg" placeholder="Enter Email" />
+						</div>
 					</div>
-				</div>
-				<div class="row d-block">
-					<label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Phone</label>
-					<div class="col-sm-10">
-						<input type="email" class="form-control form-control-lg colorStyle" style={{ width: "1235px" }} id="colFormLabelLg" placeholder="Enter phone" />
+					<div class="row d-block">
+						<label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Phone</label>
+						<div class="col-sm-10">
+							<input value={contact.phone} type="text" onChange={(event) => setContact({ ...contact, phone: event.target.value })} class="form-control form-control-lg colorStyle" style={{ width: "1235px" }} id="colFormLabelLg" placeholder="Enter phone" />
+						</div>
 					</div>
-				</div>
-				<div class="row d-block">
-					<label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Address</label>
-					<div class="col-sm-10">
-						<input type="email" class="form-control form-control-lg colorStyle" style={{ width: "1235px" }} id="colFormLabelLg" placeholder="Enter addrees" />
+					<div class="row d-block">
+						<label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Address</label>
+						<div class="col-sm-10">
+							<input value={contact.address} type="text" onChange={(event) => setContact({ ...contact, address: event.target.value })} class="form-control form-control-lg colorStyle" style={{ width: "1235px" }} id="colFormLabelLg" placeholder="Enter addrees" />
+						</div>
 					</div>
+
+					<button style={{ width: "95%", background: "lightslategray" }} type="submit" class="btn transition">Save</button>
 				</div>
 
-				<button style={{ width: "95%", background: "lightslategray" }} type="button" class="btn transition">Save</button>
-			</div>
-
-
+			</form>
 			<Link to="/" >
 				<button style={{ margin: "10px", background: "lightslategray" }} className="btn transition">Back home</button>
 			</Link>
 		</div>
-	</div>;
+
+	</>
 };
